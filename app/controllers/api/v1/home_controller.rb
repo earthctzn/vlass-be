40577efe549199fc2d8b6021769c25b743810335
earthctzn <earthctzn1@gmail.com>
2020-05-18 17:00:08 -0400

    Adds methods to home controller.

--- app/controllers/api/v1/home_controller.rb
+++ app/controllers/api/v1/home_controller.rb
@@ -1,2 +1,19 @@
 class API::V1::HomeController < ApplicationController
+
+    def create
+        home = Home.new(
+            content: home_params[:content], 
+        )
+        if home.save
+            render json: home, only: [:content], status: 200
+        else
+            render json: {error: "Error Creating home"}
+        end
+    end
+
+    def show
+        home = Home.first
+        render json: home, only: [:content], status: 200
+    end
+    
 end
