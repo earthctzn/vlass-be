617110a2d6ab4c1391a55af56096f9826d066039
earthctzn <earthctzn1@gmail.com>
2020-05-27 13:54:31 -0400

    Adds configuration for cookies in application.rb file. Updates user migration to include password_digest column.

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,7 +1,6 @@
 class Api::V1::UsersController < ApplicationController
 
     def create
-        byebug
         user = User.new(user_params)
         if user.save
             session[:user_id] = user.id
--- db/schema.rb
+++ db/schema.rb
@@ -32,7 +32,7 @@ ActiveRecord::Schema.define(version: 2020_05_26_161718) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
-    t.string "password"
+    t.string "password_digest"
   end
 
   create_table "workflows", force: :cascade do |t|
