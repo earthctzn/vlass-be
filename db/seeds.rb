995ad6edc3a2b0acea3fe288044532be7347930c
earthctzn <earthctzn1@gmail.com>
2020-05-29 16:32:00 -0400

    Adds seed data for all components.

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,14 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 home = Home.create({content: "BREAKING THE GLASS CEILING TO UNLOCK YOUR POTENTIAL"})
+about = About.create({title: "HELLO, WE ARE VLASS.", content: "AT VLASS WE WORK ON TRANSFORMING YOUR BUSINESS THROUGH WEB DESIGN, CONTENT CREATION, GRAPHIC DESIGN, AND MARKETING TO PREPARE YOUR BUSINESS FOR THE FUTURE."})
+work = Work.create([
+    {title: "WORK"}, 
+    {company: "FACEBOOK", job: "RAN MARKETING CAMPAGIN FOR MESSANGER PRODUCT."},
+    {company: "TWITTER", job: "COLLABORATED IN DESIGN OF MOBILE APP."},
+    {company: "LINKEDIN", job: "DESIGNED ALL GRAPHICS FOR MARKETING MATERIALS."},
+    {company: "IBM", job: "DESIGNED UI FOR INTERNAL TOOLING."},
+    {company: "AMAZON AWS", job: "COLLABORATED IN THE CREATION OF THEIR S3 OFFERING."}
+])
+workflow = Workflow.create({title: "WORKFLOW", content: "THESE ARE OUR STEPS TO MAKE SURE WE DELIVER RESULTS"})
+contact = Contact.create({title: "CONTACT", content: "VINCENT@VLASS.CO"})
